45b571025881f990f67fbf11cc61a3a845b83cbf
chaitoo <chaitoo.93@gmail.com>
2017-10-18 02:58:51 +0530

    improved version

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ var close_buttons = document.getElementsByClassName('close');
 for (var i = 0; i < close_buttons.length; i++) {
   close_buttons[i].addEventListener('click', function() {
     document.getElementsByClassName('bottom-nav-content-overlay')[0].style.display = 'none';
+    document.getElementsByClassName('content-overlay-container')[0].scrollLeft = 0;
     var tabs = document.getElementsByClassName('bottom-nav-tabs')
     for (var i = 0; i < tabs.length; i++) {
       tabs[i].classList.remove('active');
@@ -16,30 +17,45 @@ for (var i = 0; i < close_buttons.length; i++) {
   });
 }
 
-function smoothScroll() {
-  var tabs = document.getElementsByClassName('bottom-nav-tabs')
+function smoothScroll(e) {
+  var tabs = document.getElementsByClassName('bottom-nav-tabs');
+  var wrapper = document.getElementsByClassName('content-overlay-container')[0];
+  var device_width = window.innerWidth;
+
   for (var i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove('active');
   }
-  this.classList.add('active');
-  var wrapper = document.getElementsByClassName('content-overlay-container')[0];
 
+  e.classList.add('active');
   var k = 0;
-  if(this.id == 'deals-btn')
-    k = 315;
-  if(this.id === 'coupons-btn')
-    k = 630;
+  if(e.id == 'deals-btn')
+    k = device_width-5;
+  if(e.id === 'coupons-btn')
+    k = (device_width-5)*2;
   var n = 1;
   if(wrapper.scrollLeft > k)
     n = -1;
   var maxScroll = k;
   var i = setInterval(() => {
-    wrapper.scrollLeft += 5*n;
-    if((n>=0 && wrapper.scrollLeft >= maxScroll) || (n < 0 && wrapper.scrollLeft <= maxScroll) )
+
+    if((n>=0 && wrapper.scrollLeft >= maxScroll) || (n <= 0 && wrapper.scrollLeft <= maxScroll) )
       clearInterval(i);
+    else
+    wrapper.scrollLeft += 5*n;
   }, 2);
+  
+  e.click();
 }
 
-document.getElementById('deals-btn').addEventListener('click', smoothScroll);
-document.getElementById('coupons-btn').addEventListener('click', smoothScroll);
-document.getElementById('compare-btn').addEventListener('click', smoothScroll);
+document.getElementById('deals-btn').addEventListener('click', function(){
+  smoothScroll(this);
+  this.click();
+});
+document.getElementById('coupons-btn').addEventListener('click', function(){
+  smoothScroll(this);
+  this.click();
+});
+document.getElementById('compare-btn').addEventListener('click', function(){
+  smoothScroll(this);
+  this.click();
+});
